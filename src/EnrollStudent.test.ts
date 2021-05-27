d66ced68886c86b0167fd2082614cb973e60de2a
danielarrais <danielarraiscarvalho@gmail.com>
2021-05-26 21:16:23 -0300

    Testa se o CPF do estudante está sendo validado

--- src/EnrollStudent.test.ts
+++ src/EnrollStudent.test.ts
@@ -1,12 +1,25 @@
 import EnrollStudent from "./EnrollStudent";
 
-test("Should not enroll without valid student name", function () {
+test("Should not enroll without valid student name", () => {
     const enrollStudent = new EnrollStudent();
     const enrollmentRequest = {
         "student" : {
-            "name" : "Daniel Arrais"
+            "name" : "Daniel",
+            "cpf" : "06412721380"
         }
     }
 
     expect(() => enrollStudent.execute(enrollmentRequest)).toThrow(new Error("Invalid student name"))
+})
+
+test("Should not enroll without valid student cpf", () => {
+    const enrollStudent = new EnrollStudent();
+    const enrollmentRequest = {
+        "student" : {
+            "name" : "Daniel Arrais",
+            "cpf" : "064.127.213-90"
+        }
+    }
+
+    expect(() => enrollStudent.execute(enrollmentRequest)).toThrow(new Error("Invalid student cpf"))
 })
