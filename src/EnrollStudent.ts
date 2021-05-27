2d107eaff912140d9593ea19cb15e0452a23b30c
danielarrais <danielarraiscarvalho@gmail.com>
2021-05-26 21:18:02 -0300

    Cria métodos para validar uma requisição de matrícula

--- src/EnrollStudent.ts
+++ src/EnrollStudent.ts
@@ -6,4 +6,21 @@ export default class EnrollStudent {
             throw new Error("Invalid student name")
         }
     }
+
+    private validateEnrollmentRequest(enrollmentRequest: any) {
+        this.validateCPF(enrollmentRequest.student.cpf);
+        this.validateName(enrollmentRequest.student.name);
+    }
+
+    private validateName(name: string): void {
+        if(!/^([A-Za-z]+ )+([A-Za-z])+$/.test(name)) {
+            throw new Error("Invalid student name");
+        }
+    }
+
+    private validateCPF(cpf: string): void {
+        if (!CPFValidation.exec(cpf)) {
+            throw new Error("Invalid student cpf");
+        }
+    }
 }
