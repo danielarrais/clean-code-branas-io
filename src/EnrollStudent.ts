0222fd584f8cb26f421d56a24ea19b05027b2311
danielarrais <danielarraiscarvalho@gmail.com>
2021-05-26 21:18:15 -0300

    Validar a requisição de matrícula

--- src/EnrollStudent.ts
+++ src/EnrollStudent.ts
@@ -1,10 +1,8 @@
+import CPFValidation from "./CPFValidation";
+
 export default class EnrollStudent {
-
-
     execute(enrollmentRequest: any) {
-        if (!/^([A-Za-z]+)+([A-Za-z])+$/.test(enrollmentRequest.student.name)) {
-            throw new Error("Invalid student name")
-        }
+        this.validateEnrollmentRequest(enrollmentRequest)
     }
 
     private validateEnrollmentRequest(enrollmentRequest: any) {
