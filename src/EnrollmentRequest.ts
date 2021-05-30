cd4f81e8c78d308882890b8b525c33b1c7fe8755
danielarrais <danielarraiscarvalho@gmail.com>
2021-05-30 18:14:37 -0300

    Cria método que gera matrícula do usuário

--- src/EnrollmentRequest.ts
+++ src/EnrollmentRequest.ts
@@ -2,8 +2,21 @@ import Student from "./Student";
 
 export default class EnrollmentRequest {
     student: Student;
+    module: string;
+    level: string;
+    classe: string;
 
-    constructor(student: Student) {
+    constructor(student: Student, module: string, level: string, classe: string) {
         this.student = student;
+        this.module = module;
+        this.level = level;
+        this.classe = classe;
+    }
+
+    public generateEnrollNumber(enrollSequence: string): string {
+        const currentYear = new Date().getFullYear();
+        const enrollSequencePad = enrollSequence.padStart( 4, "0");
+
+        return `${currentYear}${this.level}${module}${this.classe}${enrollSequencePad}`
     }
 }
