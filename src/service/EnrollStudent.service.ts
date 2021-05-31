ef9e8962b1fee1b6e760701614246dd453b32db1
danielarrais <danielarraiscarvalho@gmail.com>
2021-05-30 22:14:38 -0300

    Valida validação de idade

--- src/service/EnrollStudent.service.ts
+++ src/service/EnrollStudent.service.ts
@@ -1,18 +1,22 @@
 import StudentRepository from "../repository/Student.repository";
 import EnrollmentRequest from "../model/EnrollmentRequest";
 import StudentValidation from "../validation/Student.validation";
+import EnrollmentRequestValidation from "../validation/EnrollmentRequest.validation";
 
 export default class EnrollStudentService {
     studentRepository: StudentRepository;
     studentValidation: StudentValidation;
+    enrollmentRequestValidation: EnrollmentRequestValidation;
 
     constructor() {
         this.studentRepository = new StudentRepository();
         this.studentValidation = new StudentValidation(this.studentRepository);
+        this.enrollmentRequestValidation = new EnrollmentRequestValidation();
     }
 
     execute(enrollmentRequest: EnrollmentRequest): string {
         this.studentValidation.execute(enrollmentRequest.student);
+        this.enrollmentRequestValidation.execute(enrollmentRequest)
 
         this.enroll(enrollmentRequest);
 
