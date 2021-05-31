4777880bcc044904b52a9250213f451de2f7b798
danielarrais <danielarraiscarvalho@gmail.com>
2021-05-30 22:13:59 -0300

    Cria validação de idade

--- src/validation/EnrollmentRequest.validation.ts
+++ src/validation/EnrollmentRequest.validation.ts
@@ -1,15 +1,26 @@
-import Student from "../model/Student";
 import EnrollmentRequest from "../model/EnrollmentRequest";
 import DateUtil from "../util/Date.util";
+import ModuleRepository from "../repository/Module.repository";
 
 export default class EnrollmentRequestValidation {
+    private moduleRepository: ModuleRepository
+
+    constructor() {
+        this.moduleRepository = new ModuleRepository();
+    }
 
     public execute(enrollmentRequest: EnrollmentRequest): void {
+        const student = enrollmentRequest.student;
 
+        this.validateMinimumAge(student.birthDate, enrollmentRequest.module);
     }
 
     validateMinimumAge(studentBirthDate: Date, muduleCode: string) {
-        const mudule = "";
+        const minimumAgeOfModule = this.moduleRepository.findMinimumAgeByCode(muduleCode);
         const age = DateUtil.calculateAge(studentBirthDate)
+
+        if (!minimumAgeOfModule || age < minimumAgeOfModule) {
+            throw new Error("Should not enroll student below minimum age")
+        }
     }
 }
