6488993ced9b5ea07fd91007debcc34a622f0d0c
danielarrais <danielarraiscarvalho@gmail.com>
2021-06-12 17:29:12 -0300

    Cria método para validar capacidade e adiciona métodos de validação do estudante

--- src/validation/EnrollmentRequest.validation.ts
+++ src/validation/EnrollmentRequest.validation.ts
@@ -1,26 +1,61 @@
 import EnrollmentRequest from "../dto/EnrollmentRequest";
 import DateUtil from "../util/Date.util";
+import ModuleRepository from "../repository/ModuleRepositoryMemory";
 import ModuleRepositoryMemory from "../repository/ModuleRepositoryMemory";
+import ClassroomRepository from "../repository/ClassroomRepositoryMemory";
 import ClassroomRepositoryMemory from "../repository/ClassroomRepositoryMemory";
+import CPFValidation from "./CPF.validation";
+import EnrollStudentRepository from "../repository/EnrollStudentRepositoryMemory";
+import EnrollStudentRepositoryMemory from "../repository/EnrollStudentRepositoryMemory";
 
 export default class EnrollmentRequestValidation {
-    private moduleRepository: ModuleRepositoryMemory
-    private classeRepository: ClassroomRepositoryMemory
+    private moduleRepository: ModuleRepository;
+    private classroomRepository: ClassroomRepository;
+    private enrollStudentRepository: EnrollStudentRepository;
 
     constructor() {
         this.moduleRepository = new ModuleRepositoryMemory();
-        this.classeRepository = new ClassroomRepositoryMemory();
+        this.classroomRepository = new ClassroomRepositoryMemory();
+        this.enrollStudentRepository = new EnrollStudentRepositoryMemory();
     }
 
     public execute(enrollmentRequest: EnrollmentRequest): void {
         const student = enrollmentRequest.student;
 
+        this.validateCPF(student.cpf);
+        this.validateName(student.name);
+        this.validateUnique(student.cpf);
         this.validateMinimumAge(student.birthDate, enrollmentRequest.module, enrollmentRequest.level);
-        // this.validateCapacityOfClasse(enrollmentRequest.classe);
+        this.validateCapacityOfClasse(enrollmentRequest.classroom, enrollmentRequest.module, enrollmentRequest.level);
     }
 
-    validateCapacityOfClasse() {
+    private validateUnique(cpf: string): void {
+        if (this.enrollStudentRepository.existByCpf(cpf)) {
+            throw new Error("Enrollment with duplicated student is not allowed");
+        }
+    }
 
+    private validateName(name: string): void {
+        if (!/^([A-Za-z]+ )+([A-Za-z])+$/.test(name)) {
+            throw new Error("Invalid student name");
+        }
+    }
+
+    private validateCPF(cpf: string): void {
+        if (!CPFValidation.exec(cpf)) {
+            throw new Error("Invalid student cpf");
+        }
+    }
+
+    validateCapacityOfClasse(classroom: string, module: string, level: string) {
+        const classroomCapacity = this.classroomRepository.findCapacityByCodeAndModuleAndLevel(classroom, level, module)
+        const actualNumberOfStudents = this.enrollStudentRepository.countByClassroomAndModuleAndLevel(classroom, level, module)
+
+        console.log(actualNumberOfStudents + ' - ' + classroomCapacity);
+
+        if (actualNumberOfStudents > classroomCapacity) {
+            throw new Error("Class is over capacity")
+        }
     }
 
     validateMinimumAge(studentBirthDate: Date, muduleCode: string, level: string) {
